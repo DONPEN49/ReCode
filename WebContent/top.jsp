0daae5cd45c39400129276f4fedfc0aa68929201
Hotaka Kitamura <hotaka0409@icloud.com>
2023-06-01 17:18:45 +0900

    【追加】基本セット導入

---
+++ WebContent/top.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%!
+	private String recodes;
+%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="style.css">
+<title>ReCode</title>
+</head>
+<body>
+	<header><h1>みんなの投稿</h1><hr></header>
+	<main>
+		<div id = "recode">
+
+			<h2>
+				<%=request.getAttribute("message") %>
+			</h2>
+
+		</div>
+
+		<section>
+			<form action="controller" method="post">
+				<input type="text" name="recode">
+				<input type="submit" value="送信">
+			</form>
+		</section>
+	</main>
+</body>
+</html>
---
+++ src/servlet/ControllerServlet.java
@@ -0,0 +1,55 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ControllerServlet
+ */
+@WebServlet("/controller")
+public class ControllerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setCharacterEncoding("UTF-8");
+		ServletContext context = getServletContext();
+		RequestDispatcher dispatcher = context.getRequestDispatcher("/top.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		request.setCharacterEncoding("UTF-8");
+		String recode = request.getParameter("recode");
+		String message;
+		if (recode != null) {
+			message = recode;
+		} else {
+			message = "何も投稿がありません";
+		}
+
+		request.setAttribute("message", message);
+
+		ServletContext context = getServletContext();
+		RequestDispatcher dispatcher = context.getRequestDispatcher("/top.jsp");
+		dispatcher.forward(request, response);
+
+	}
+
+}
